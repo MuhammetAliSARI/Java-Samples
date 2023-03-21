3b199b53af82e0e9e61d4bf6c1b7a027faf27656
Muhammet Ali SARI <122355423+horonaga@users.noreply.github.com>
2023-03-21 22:10:12 +0300

    Update and rename Selenium-Poolxml.java to Selenium-Edge-Poolxml.java

---
+++ Selenium-Edge-Poolxml.java
@@ -0,0 +1,14 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class C01_Selenium {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resource/drivers/msedgedriver.exe");
+
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
