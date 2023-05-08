aa83d82663ff93e74f24ea0cceedebe7a73133dc
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-08 22:56:08 +0300

    Create Klasor_Olustur.java

---
+++ Klasor_Olustur.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+public class KlasorOlustur {
+    public static void main(String[] args) {
+        String folderPath = "C:/Yeni Klasör";
+        File folder = new File(folderPath);
+
+        if (!folder.exists()) {
+            boolean isCreated = folder.mkdirs();
+
+            if (isCreated) {
+                System.out.println("Klasör oluşturuldu.");
+            } else {
+                System.out.println("Klasör oluşturulamadı.");
+            }
+        } else {
+            System.out.println("Klasör zaten mevcut.");
+        }
+    }
+}
+
+// Hazırlayan : Emre Demirtekin
