3a26dacefa1ffd19d3c10aa6504cd48f7cf4db86
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-25 23:01:28 +0300

    Create HaftaninGunleriRunner.java

---
+++ HaftanınGunleri/HaftaninGunleriRunner.java
@@ -0,0 +1,10 @@
+package horonaga.HaftanınGunleri;
+
+public class HaftaninGunleriRunner {
+
+    public static void main(String[] args) {
+
+        System.out.print(HaftaninGunleri.Cumartesi.getGunAdi() + " " + HaftaninGunleri.Cumartesi.getGunSayisi());
+
+    }
+}
