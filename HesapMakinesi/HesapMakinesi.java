6438e655aa76c60f8dc50f39b64164bbfc203c22
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-25 23:02:18 +0300

    Create HesapMakinesi.java

---
+++ HesapMakinesi/HesapMakinesi.java
@@ -0,0 +1,17 @@
+package horonaga.HesapMakinesi;
+
+public class HesapMakinesi {
+    public int Topla(int sayi1, int sayi2){     // Toplama işlemi
+        return sayi1 + sayi2;
+    }
+    public int Cikar(int sayi1, int sayi2) {    // Çıkarma işlemi
+        return sayi1 - sayi2;
+    }
+    public int Carp(int sayi1, int sayi2) {     // Çarpma işlemi
+        return sayi1 * sayi2;
+    }
+    public int Bol(int sayi1, int sayi2) {      // Bölme işlemi
+        return sayi1 / sayi2;
+    }
+
+}
