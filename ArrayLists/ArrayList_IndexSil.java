70a6415df954fa884a33df2c94630f92b9ee6773
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-25 23:27:32 +0300

    Create ArrayList_IndexSil.java

---
+++ ArrayLists/ArrayList_IndexSil.java
@@ -0,0 +1,54 @@
+package horonaga.Metodhs.ArraysLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_IndexSil {
+    public static void main(String[] args) {
+        List<Integer> ages = new ArrayList<>();
+        ages.add(10);
+        ages.add(25);
+        ages.add(12);
+        ages.add(8);
+        ages.add(40);
+
+        //1.Yol
+        //Integer nonPrimitive = 12;
+        //ages.remove(nonPrimitive);
+
+        //2.Yol: Recommended
+        //ages.remove((Integer)12);
+
+        //3.Yol
+        //ages.remove(Integer.valueOf(12));// 2 method kullanildi
+
+        //4.Yol
+        //ages.remove(ages.indexOf(12));// 2 method kullanildi
+
+        System.out.println(ages);
+
+        //Example 2: Integer bir List olusturunuz
+        //           List'e 5 tane eleman ekleyiniz
+        //           Tum 12'leri siliniz.
+        //
+        List<Integer> nums = new ArrayList<>();
+        nums.add(10);
+        nums.add(25);
+        nums.add(12);
+        nums.add(8);
+        nums.add(12);
+
+        List<Integer> silinecekler = new ArrayList<>();
+        silinecekler.add(12);
+        silinecekler.add(25);
+
+        //removeAll() method'u List ile calisir.
+        //removeAll() method'u bir veya birden fazla elemanin tum gorunumlerini silmek icin kullanilir.
+        nums.removeAll(silinecekler);
+
+        System.out.println(nums);
+
+
+
+    }
+}
