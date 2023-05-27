99df1860e0aa0d2382d2c0f0fed6830aff41f519
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 23:50:06 +0300

    Create Varargs_IndexElemanlariToplami.java

---
+++ Varargs/Varargs_IndexElemanlariToplami.java
@@ -0,0 +1,29 @@
+package horonaga.Metodhs.Varargs;
+
+public class Varargs_IndexElemanlariToplami {
+
+    //INTERWIEW SORUSU
+    //Aşağıdaki multidimensional array'lerin iç array'lerinde
+    // aynı indexe sahip elamanların toplamını yazdıran bir program yazın.
+    // int arr1[][] = {{1, 2},     { 3, 4, 5},{6}};
+    // int arr2[][] = {{7, 8,9},  {12}};
+
+    public static void main(String[] args) {
+        int sinir=0;
+        int toplam=0;
+        int arr1[][] = {{1, 2},     { 3, 4, 5},{6}};
+        int arr2[][] = {{7, 8,9},  {12}};
+
+        for (int i = 0; i < arr2.length ; i++) {//kisa olan kat alindi
+            sinir=arr1[i].length;
+            if(arr1[i].length> arr2[i].length){
+                sinir=arr2[i].length;
+            }//buraya kadar sinirlari buldum
+
+            for (int j = 0; j <sinir ; j++) {
+                toplam=arr1[i][j]+arr2[i][j];
+                System.out.println("["+i+","+j+"]"+" indexindeki elemanlarin toplami "+toplam);
+            }
+        }
+    }
+}
