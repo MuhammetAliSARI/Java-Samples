131cc95840ac3fdfeb9b7763bb7b91ac6ffe3e23
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 23:49:27 +0300

    Create Varargs_HaricCarp.java

---
+++ Varargs/Varargs_HaricCarp.java
@@ -0,0 +1,27 @@
+package horonaga.Metodhs.Varargs;
+
+public class Varargs_HaricCarp {
+    // Verilen sayilardan ilki haric tum sayilari
+    // toplayip ilk sayi ile carpan bir method olusturunuz
+    public static void main(String[] args) {
+        int sayi1=5;
+        int sayi2=7;
+        int sayi3=2;
+        int sayi4=4;
+        int sayi5=9;
+
+        sonuc(sayi1,sayi2,sayi3,sayi4,sayi5);
+    }//main
+    private static void sonuc(int carpilacakSayi,int... toplanacakSayi) {
+
+        int toplam=0;
+
+        for (int w : toplanacakSayi) {
+            toplam += w;
+        }
+        System.out.println("toplam = " + toplam);//22
+
+        int sonuc=toplam*carpilacakSayi;
+        System.out.println("sonuc = " + sonuc);//110
+    }//method
+}//class
