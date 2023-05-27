8902dbead20077e1df8809ec1d3366e2f3447815
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 23:22:35 +0300

    Create Multidimensional_Yildiz_Kulesi.java

---
+++ Input/Arrays_Multidimensional/Multidimensional_Yildiz_Kulesi.java
@@ -0,0 +1,33 @@
+package horonaga.Metodhs.MultidimensionalArrays;
+
+public class Multidimensional_Yildiz_Kulesi {
+    public static void main(String[] args) {
+
+        /*
+            Asagidaki sekli cizen kodu yaziniz
+                                *
+                               * *
+                              *   *
+                             *     *
+                            * * * * *
+         */
+        int row = 5;
+
+        for(int i=1; i<=row; i++){
+
+            for(int k=i; k<row; k++){
+                System.out.print(" ");
+            }
+
+            for(int m=1; m<=2*i-1; m++){
+
+                if(i==row || m==1 || m==2*i-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
