f35874b16bdc49167ef2000846ab27c41c2a2170
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 23:47:19 +0300

    Create HesapMakinesiVarargs.java

---
+++ Varargs/HesapMakinesiVarargs.java
@@ -0,0 +1,11 @@
+package horonaga.Metodhs.Varargs;
+
+public class HesapMakinesiVarargs {
+    public static void main(String[] args) {
+        HesapMakinesi islem = new HesapMakinesi();
+        islem.toplama(3,5,7,96,54,-85);
+        islem.cikarma(10,10,0,40);
+        islem.carpma(15,63,-8);
+        islem.bolme(3,5);
+    }
+}
