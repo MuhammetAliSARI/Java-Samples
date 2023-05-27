e79df1a296760c66a8e77ba5019025b9fea8043e
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 22:57:31 +0300

    Create For_Ic_Ice.java

---
+++ For/For_Ic_Ice.java
@@ -0,0 +1,12 @@
+package horonaga.Metodhs.For;
+
+public class For_Ic_Ice {
+    public static void main(String[] args) {
+        for(int i=1; i<5;i++){
+            System.out.println("Bölge: " + i);
+            for (int k=1;k<8;k++){
+                System.out.println("İl: " + k);
+            }
+        }
+    }
+}
