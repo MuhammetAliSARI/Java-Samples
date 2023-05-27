40493af55ba89f11b97ad5c45b58a184b25e2e04
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 23:46:10 +0300

    Create VarargsOzellikler.java

---
+++ Varargs/VarargsOzellikler.java
@@ -0,0 +1,18 @@
+package horonaga.Metodhs.Varargs;
+public class VarargsOzellikler {
+    /*
+ 1) Bir metotta parametre sayısını esnek hale getirmek istiyorsanız, "varargs" kullanın.
+ 2) Varargs oluşturmak için "Veri Tipi + ... + boşluk + varargslar için bir isim"
+ 3) "Varargs", arkasında Array kullanır. Varargs ile çalışırken Array ile çalıştığınızı düşünebilirsiniz.
+ 4) "Varargs" sıfır veya daha fazla öğe kabul edebilir
+ 5)"Varargs"dan sonra başka bir normal parametre kullanabilir miyim?
+ Hayır, "Varargs" tam yapılamayacağı için ikinci parametreye ulaşılamayacak, hata verecektir.
+ "Varargs" her zaman son parametre olmalıdır.
+
+ 6)"Varargs"dan önce başka bir normal parametre kullanabilir miyim?
+ Evet, "Varargs" dan önce birçok normal parametre kullanabilirsiniz.
+
+ 7)Bir metotta birden fazla "Varargs" parametresi kullanabilir miyim?
+         Hayir .Cunku sondaki Varargs'a unreachable code olur
+         */
+}
