f2859abb260de80c4c0b9b90b4f88f7835a8f5f0
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 22:57:01 +0300

    Create For_Harika.java

---
+++ For/For_Harika.java
@@ -0,0 +1,13 @@
+package horonaga.Metodhs.For;
+
+public class For_Harika {
+    public static void main(String[] args) {
+        String metin1 = "Merhaba Dünya";
+            for ( int i=0 ;i<metin1.length() ; i++) { if (metin1.charAt(i) != 'a')
+            {System.out.print(metin1.charAt(i));}} // VERİLEN BİR STRİGNG'DE İLK "a" HARFİNDEN ÖNCEKİ TÜM KARAKTERLERİ YAZDIR.
+
+        String metin2 = "Bize her yer Trabzon";
+            for ( int e = metin2.length()-1; e>=0; e--){ if(metin2.charAt(e)=='a') { break;}
+            { System.out.print(metin2.charAt(e));}} // VERİLEN BİR STRİNG'DE SON 'a' HARFİNDEN SONRAKİ TÜM KARAKTERLERİ EKRANA YAZDIR.
+    }
+}
