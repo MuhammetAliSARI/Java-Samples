a1a6932d56a0d8f50da492718bae68d2decec481
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 23:40:00 +0300

    Create StaticOrnek_04.java

---
+++ Static/StaticOrnek_04.java
@@ -0,0 +1,19 @@
+/* package horonaga.Metodhs.Static;
+
+public class StaticOrnek_04 {
+    static int sayi1 = 20;
+    int sayi2 = 20;
+
+    public static void main(String[] args) {
+        System.out.println(sayi1);
+        System.out.println(sayi2);
+    }
+
+    public static void add(String[] args) {
+        System.out.println(sayi1);
+        System.out.println(sayi2);
+
+    }
+}
+// Görüldüğü üzere sayi2 statik olmadığı için algılanmıyor
+*/
