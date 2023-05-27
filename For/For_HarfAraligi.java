8b1ff15f06784899516f19005b03687a37d751f5
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 22:56:36 +0300

    Create For_HarfAraligi.java

---
+++ For/For_HarfAraligi.java
@@ -0,0 +1,9 @@
+package horonaga.Metodhs.For;
+//m ile c arasındaki harfleri ekrana ver
+public class For_HarfAraligi {
+    public static void main(String[] args) {
+        for (char i='m'; i>='c'; i--){
+            System.out.print(i);
+        }
+    }
+}
