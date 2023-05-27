4965413c0ed5d8f7a86707fa6cad11596ed9e039
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 22:54:59 +0300

    Create EndsWithIleMiBiter.java

---
+++ endsWith/EndsWithIleMiBiter.java
@@ -0,0 +1,11 @@
+package horonaga.Metodhs.EndsWithx;
+
+public class EndsWithIleMiBiter {
+    public static void main(String[] args) {
+        String s = "Maraba Televole";
+
+        boolean ileMiBiter = s.endsWith("vole");  //endWith metodu ile bitişi kontrol edeceğiz.
+        System.out.println(ileMiBiter);
+
+    }
+}
