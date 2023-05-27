72c5a637aedd731665cdffdb7eac765ac0650545
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-27 22:59:06 +0300

    Create For_Ilk.java

---
+++ For/For_Ilk.java
@@ -0,0 +1,9 @@
+package horonaga.Metodhs.For;
+// ALT ALTA 5 KERE "ALİ" YAZSIN
+public class For_Ilk {
+    public static void main(String[] args) {
+        for(int i=1;i<6;i++){
+            System.out.println("Ali " + i);
+        }
+    }
+}
