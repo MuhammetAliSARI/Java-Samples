3d309364af0b8603840b833e1e930e2fdfecb6bf
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 16:44:17 +0300

    Create StringBuilder_Reverse_EnBasit.java

---
+++ StringBuilder/StringBuilder_Reverse_EnBasit.java
@@ -0,0 +1,9 @@
+package horonaga;
+
+public class StringBuilder_Reverse_EnBasit {
+    public static void main(String[] args) {
+        String a = "Trabzon";
+        StringBuilder b = new StringBuilder(a);
+        System.out.println(b.reverse());
+    }
+}
