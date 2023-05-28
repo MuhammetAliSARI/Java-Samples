1533ef84dca315429ae537e563f513217c9a224f
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 16:42:57 +0300

    Create StaticMember.java

---
+++ StaticMember/StaticMember.java
@@ -0,0 +1,21 @@
+package horonaga;
+
+public class StaticMember {//StaticUyeler
+
+    //Ekran Ciktisi Nedir?
+    static int x;
+    int y;
+    StaticMember(){//constructor
+        x+=10;
+        y++;
+    }//const
+    public static int karesiniAl(){
+        return x*x;
+    }
+    public static void main(String[] args) {
+        StaticMember sm1 = new StaticMember();
+        StaticMember sm2 = new StaticMember();
+        int z=sm1.karesiniAl();
+        System.out.println("-x "+z+" -y"+sm2.y);//-x 400 -y1
+    }//main
+}//class
