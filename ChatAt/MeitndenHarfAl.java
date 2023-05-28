9792ada348e2778389f05e53fc3118e6cec08999
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 15:49:31 +0300

    Create MeitndenHarfAl.java

---
+++ ChatAt/MeitndenHarfAl.java
@@ -0,0 +1,10 @@
+package horonaga;
+
+public class MeitndenHarfAl {
+    public static void main(String[] args) {
+        String metin = "Bu bir metindir.";
+
+        char ch = metin.charAt(3);
+        System.out.println(ch);
+    }
+}
