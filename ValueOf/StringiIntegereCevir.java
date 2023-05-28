896db61efca340d1880ec22388514c66f985f8fa
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 16:45:26 +0300

    Create StringiIntegereCevir.java

---
+++ ValueOf/StringiIntegereCevir.java
@@ -0,0 +1,9 @@
+package horonaga;
+
+public class StringiIntegereCevir {
+    public static void main(String[] args) {
+        String s = "1234";
+        int intHali = Integer.valueOf(s);
+        System.out.println(intHali + 766);  // 2000
+    }
+}
