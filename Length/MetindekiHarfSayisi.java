9b003a0f3b3a36e320280b93d19fa0400b676017
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 15:54:30 +0300

    Create MetindekiHarfSayisi.java

---
+++ Length/MetindekiHarfSayisi.java
@@ -0,0 +1,12 @@
+package horonaga;
+
+// VERİLEN METİNDEKİ HARF SAYISINI BULACAĞIZ
+
+public class MetindekiHarfSayisi {
+    public static void main(String[] args) {
+        String s = " Muhammet Ali Sari";
+
+        int sLength = s.length();
+        System.out.println(sLength);
+    }
+}
