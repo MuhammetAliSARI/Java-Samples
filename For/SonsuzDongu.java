39dfc49f8c3e8b31da7bf255c8537d52a0759875
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 16:42:06 +0300

    Create SonsuzDongu.java

---
+++ For/SonsuzDongu.java
@@ -0,0 +1,12 @@
+package horonaga;
+// SONSUZ / INFINITE BİR LOOP OLUŞTUR
+
+public class SonsuzDongu {
+    public static void main(String[] args) {
+
+        for(int i = 0 ; i<1 ; ){
+            System.out.println("sonsuz");
+        }
+    }
+}
+// Bu, esasında hatalı bir koddur.
