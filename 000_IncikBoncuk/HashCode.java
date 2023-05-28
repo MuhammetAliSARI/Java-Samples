6a4d537ac34886cb0d98e8c416f34e4d4702c130
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 15:24:49 +0300

    Create HashCode.java

---
+++ 000_IncikBoncuk/HashCode.java
@@ -0,0 +1,8 @@
+package horonaga;
+// JAVA'NIN KENDİSİNE HAS KOD OLUŞTURMA BÇİMİDİR. Bu bir hashing tekniktir.
+public class HashCode {
+    public static void main(String[] args) {
+        String metin = "Bu bir metindir.";
+        System.out.println(metin.hashCode());
+    }
+}
