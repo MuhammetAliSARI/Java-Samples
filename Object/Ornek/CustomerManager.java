be0d659bd07ed8063df79365af97ff90eaae2833
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 14:17:13 +0300

    Create CustomerManager.java

---
+++ Object/Ornek/CustomerManager.java
@@ -0,0 +1,13 @@
+package horonaga.ObjeOlusturma;
+
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Müşteri eklendi");
+    }
+    public void Remove(){
+        System.out.println("Müşteri silidi");
+    }
+    public void Update(){
+        System.out.println("Müşteri güncellendi");
+    }
+}
