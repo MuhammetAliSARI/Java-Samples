5c0142cd7a7d5b2c81dbd94646762f3148ac0c0c
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 16:48:18 +0300

    Create Topla.java

---
+++ 000_IncikBoncuk/Topla.java
@@ -0,0 +1,13 @@
+package horonaga;
+// BOSLUK SİL
+public class Topla {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 1;
+        b = 2;
+        c = 3;
+        System.out.println(a+b+c);
+    }
+}
+
+// trim() metodu; aradaki boşluklara dokunmadan sadece baş ve sondaki boşlukları siler.
