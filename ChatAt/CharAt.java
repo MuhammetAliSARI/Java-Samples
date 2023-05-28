29a2f6aedc0136cc5c8582882fdc5587d2fa699f
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 14:00:36 +0300

    Create CharAt.java

---
+++ ChatAt/CharAt.java
@@ -0,0 +1,11 @@
+package horonaga.Metodhs;
+// BİR STRİNG'TEN BELLİ BİR İNDEXTEKİ KARAKTERİ ALMAK İÇİN KULLANILIR.
+public class CharAt {
+    public static void main(String[] args) {
+        String metin = "Merhaba dünya";
+        System.out.println(metin.charAt(8)); // "Merhaba Metninin 8. karakteri ekrana yazılır.
+    }
+}
+
+// Boşluğu da sayar
+// İlk karakterin değeri sıfırdır.
