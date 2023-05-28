203019596a3e428d5f413eef8dc3d5099590e084
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 14:52:25 +0300

    Create SayiyiKucukBuyugeDonustur.java

---
+++ SayiyiKucukBuyugeDonustur.java
@@ -0,0 +1,14 @@
+package horonaga;
+
+public class SayiyiKucukBuyugeDonustur {
+    public static void main(String[] args) {
+        int age = 138000;
+        System.out.println(age);
+        float ageInt = age;
+        System.out.println(ageInt);
+    }
+}
+
+// BURADA NE OLDU? int tipindeki veri, float tipine dönüştürüldü.
+// Burada gemişletme (Autowidening) işlemi olmuştu.
+
