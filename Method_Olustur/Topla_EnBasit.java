52669335a3c4d31ce52605ff096b44324726c484
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 16:50:10 +0300

    Create Topla_EnBasit.java

---
+++ Method_Olustur/Topla_EnBasit.java
@@ -0,0 +1,11 @@
+package horonaga;
+
+public class Topla_EnBasit {
+    public static void main(String[] args) {
+        topla(3,5);
+
+    }
+    public static void topla(int a, int b){
+        System.out.println(a+b);
+    }
+}
