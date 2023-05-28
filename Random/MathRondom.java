3d11da3c6da2fccce0509765ae019096418f60ca
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 15:46:58 +0300

    Create MathRondom.java

---
+++ Random/MathRondom.java
@@ -0,0 +1,10 @@
+package horonaga;
+// Math.random() METODU 0 İLE 1 ARASINDA ( 0 dahil, 1 hariç) BİR SAYI VERİR.
+public class MathRondom {
+    public static void main(String[] args) {
+        System.out.println(Math.random());
+
+    }
+}
+
+// Bu metod her defasında farklı bir değer verir.
