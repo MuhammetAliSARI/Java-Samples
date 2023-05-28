d5e1d2fae2883a544d3f7d3626ec23e522f4bd34
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 16:46:01 +0300

    Create Substring.java

---
+++ Substring/Substring.java
@@ -0,0 +1,9 @@
+package horonaga;
+// 'den sonrasını al
+public class Substring {
+    public static void main(String[] args) {
+        String cumle = "Bize her yer Trabzon";
+        System.out.println(cumle.substring(14));
+        // 10.karakterden sonraki metni de bu yıldızlı metnin devamına ekledik.
+    }
+}
