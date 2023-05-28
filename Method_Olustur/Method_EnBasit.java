d8fc879e8a6c447cb755adaf675a47154947f7be
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-28 15:53:12 +0300

    Create Method_EnBasit.java

---
+++ Method_Olustur/Method_EnBasit.java
@@ -0,0 +1,10 @@
+package horonaga;
+
+public class Method_EnBasit {
+    public static void main(String[] args) {
+        printAge(35);
+    }
+    public static void printAge(int age){
+        System.out.println(age);
+    }
+}
