7d894ddbb2c6ba68d6898b150c1861732b7c5855
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-05-31 14:50:28 +0300

    Update For_Harika.java

--- For/For_Harika.java
+++ For/For_Harika.java
@@ -4,10 +4,10 @@ public class For_Harika {
     public static void main(String[] args) {
         String metin1 = "Merhaba Dünya";
             for ( int i=0 ;i<metin1.length() ; i++) { if (metin1.charAt(i) != 'a')
-            {System.out.print(metin1.charAt(i));}} // VERİLEN BİR STRİGNG'DE İLK "a" HARFİNDEN ÖNCEKİ TÜM KARAKTERLERİ YAZDIR.
+            {System.out.print(metin1.charAt(i));}}          // VERİLEN BİR STRİGNG'DE İLK "a" HARFİNDEN ÖNCEKİ TÜM KARAKTERLERİ YAZDIR.
 
         String metin2 = "Bize her yer Trabzon";
             for ( int e = metin2.length()-1; e>=0; e--){ if(metin2.charAt(e)=='a') { break;}
-            { System.out.print(metin2.charAt(e));}} // VERİLEN BİR STRİNG'DE SON 'a' HARFİNDEN SONRAKİ TÜM KARAKTERLERİ EKRANA YAZDIR.
+            { System.out.print(metin2.charAt(e));}}         // VERİLEN BİR STRİNG'DE SON 'a' HARFİNDEN SONRAKİ TÜM KARAKTERLERİ EKRANA YAZDIR.
     }
 }
