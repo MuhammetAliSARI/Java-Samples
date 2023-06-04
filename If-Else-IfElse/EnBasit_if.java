d44b8f50bf8b036fe31817a6c1c75b0c8415b33c
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-06-04 21:20:27 +0300

    Create EnBasit_if.java

---
+++ If-Else-IfElse/EnBasit_if.java
@@ -0,0 +1,12 @@
+public class ifEnBasit {
+    public static void main(String[] args) {
+        boolean varMi=false;
+        if (varMi){
+            System.out.println("Sayı mevcuttur.");
+        }else{
+            System.out.println("Sayı mevcut değildir.");
+        }
+
+        // Eğer varMi değerini true girmiş olsaydık, ekrana "Sayı mevcuttur" gelecekti.
+    }
+}
