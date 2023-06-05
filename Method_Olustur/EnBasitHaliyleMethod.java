c653f891d429aee7964e673fc7d81612df729f99
Muhammet Ali SARI <122355423+MuhammetAliSARI@users.noreply.github.com>
2023-06-05 07:08:39 +0300

    Create EnBasitHaliyleMethod.java

---
+++ Method_Olustur/EnBasitHaliyleMethod.java
@@ -0,0 +1,9 @@
+public class EnBasitHaliyleMethod {
+    public static void main(String[] args) {
+        int sayi = topla(5,6);
+        System.out.println(topla(9,8));
+    }
+    public static int topla (int a,int b){
+        return a+b;
+    }
+}
